d475aa992efa5effcf2d2ce9b3b0da6dc322a90b
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-06-07 04:52:14 +0530

    ran formatter (prettier extension) through relevant files

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import React from 'react';
-import { useLongPress } from 'use-long-press';
-import ToDoLists from './ToDoLists';
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import React from "react";
+import { useLongPress } from "use-long-press";
+import ToDoLists from "./ToDoLists";
 
 const App = () => {
-  const [inputList, setInputList] = useState("");
-  const [Items, setItems] = useState([]);
-  const itemEvent = (event) =>{
-    setInputList(event.target.value);
-  };
+	const [inputList, setInputList] = useState("");
+	const [Items, setItems] = useState([]);
+	const itemEvent = (event) => {
+		setInputList(event.target.value);
+	};
 
-  const listOfItems=()=>{
-    setItems((oldItems)=>{
-      return [...oldItems, inputList]
-    })
-    setInputList('');
-  };
+	const listOfItems = () => {
+		setItems((oldItems) => {
+			return [...oldItems, inputList];
+		});
+		setInputList("");
+	};
 
-  const deleteItems=(id)=>{
-    console.log("DED");
-    setItems((oldItems)=>{
-      return oldItems.filter((arrElem, index)=>{
-        return index !==id;
-      })
-    })
-}
+	const deleteItems = (id) => {
+		console.log("DED");
+		setItems((oldItems) => {
+			return oldItems.filter((arrElem, index) => {
+				return index !== id;
+			});
+		});
+	};
 
-  return(
-    <div className='main_div'>
-        <div className="center_div"><br />
-        <h1 className="text-3xl font-bold ">ToDo List</h1>
-        <br />
-        <input type="text" placeholder='Add a task' name="" id="" onChange={itemEvent} value={inputList}/>
-        <button onClick={listOfItems}> +</button>
+	return (
+		<div className="main_div">
+			<div className="center_div">
+				<br />
+				<h1 className="text-3xl font-bold ">ToDo List</h1>
+				<br />
+				<input
+					type="text"
+					placeholder="Add a task"
+					name=""
+					id=""
+					onChange={itemEvent}
+					value={inputList}
+				/>
+				<button onClick={listOfItems}> +</button>
 
-        <ol>
-         {/* <li>{inputList}</li> */}
-         {Items.map( (itemval, index) => {
-         return <ToDoLists 
-         key = {index}
-         id = {index}
-         text = {itemval}
-         onSelect = {deleteItems} />;
-        })}
-        </ol>
-      </div>
-    </div>
-  )
+				<ol>
+					{/* <li>{inputList}</li> */}
+					{Items.map((itemval, index) => {
+						return (
+							<ToDoLists
+								key={index}
+								id={index}
+								text={itemval}
+								onSelect={deleteItems}
+							/>
+						);
+					})}
+				</ol>
+			</div>
+		</div>
+	);
 };
 
-export default App
+export default App;
--- src/ToDoLists.jsx
+++ src/ToDoLists.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import {BsFillXCircleFill} from "react-icons/bs";
-import { useLongPress } from 'use-long-press';
+import { BsFillXCircleFill } from "react-icons/bs";
+import { useLongPress } from "use-long-press";
 
 const ToDoLists = (props) => {
-    
-    return (
-    <>
-        <div className="todo_style">
-            <i className="icon" onClick={()=>{props.onSelect(props.id)}}><BsFillXCircleFill/></i>
-            <li>{props.text}</li>
-        </div>
-    </>
-    );
+	return (
+		<>
+			<div className="todo_style">
+				<i
+					className="icon"
+					onClick={() => {
+						props.onSelect(props.id);
+					}}
+				>
+					<BsFillXCircleFill />
+				</i>
+				<li>{props.text}</li>
+			</div>
+		</>
+	);
 };
 
 export default ToDoLists;
